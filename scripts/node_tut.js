b9ca8cbf1be9e7b7f5d9cf0910888a5074297281
Zac <zdobbs3@gmail.com>
2019-02-13 09:11:13 -0700

    added request to node_modules

--- scripts/node_tut.js
+++ scripts/node_tut.js
@@ -1,5 +1,6 @@
 let request = require('request');
 
 request('https://docs.google.com/spreadsheets/d/1WtbKvtBszbpJ-DDsCDWSh33p0NrWPQGKx-D2MVdZBQs/edit', function(error, response, body){
+  console.log('statusCode:', response && response.statusCode);
   console.log('body: ', body);
 })
