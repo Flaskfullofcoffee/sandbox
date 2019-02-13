ee4d60b3a981c9d77d6a37d9946dabb58abd6912
Zac <zdobbs3@gmail.com>
2019-02-13 09:28:07 -0700

    added requirejs to devDependencies

--- scripts/node_tut.js
+++ scripts/node_tut.js
@@ -1,6 +1,18 @@
-let request = require('request');
+const https = require('https');
 
-request('https://docs.google.com/spreadsheets/d/1WtbKvtBszbpJ-DDsCDWSh33p0NrWPQGKx-D2MVdZBQs/edit', function(error, response, body){
-  console.log('statusCode:', response && response.statusCode);
-  console.log('body: ', body);
-})
+https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).explanation);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
